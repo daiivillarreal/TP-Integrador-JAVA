3612599580ceabdc490d4188d859681b4b406668
Daiana Villarreal <daiu.ale.07@gmail.com>
2023-12-19 03:24:44 -0300

    Update integrador_cac.sql

--- integrador_cac.sql
+++ integrador_cac.sql
@@ -5,12 +5,11 @@ CREATE TABLE `oradores` (
   `id_orador` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
-  `mail` varchar(45) NOT NULL,
   `tema` varchar(20) NOT NULL,
   `fecha_alta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) 
 
-INSERT INTO `oradores` (nombre,apellido,mail,tema) VALUES ('Nicolas','Gomez','nico@mailinator.com','JavaScript'),('Martin','Gonzalez','marting@mailinator.com','React'),('Micaela','Perez','mica_perez@mailinator.com','NodeJS'),('Erica','Garcia','erica@mailinator.com','Bases de Datos'),('Rocio','Villalba','rvillalba@mailinator.com','Angular'),('Facundo','Rios','facuR@mailinator.com','.Net'),('Federico','Acosta','fedeac@mailinator.com','Java'),('Camila','Martinez','camimar@mailinator.com','Java'),('Noelia','Sanchez','sancheznoe@mailinator.com','React'),('Daiana','Villarreal','daiu.ale.07@gmail.com','Java');
+INSERT INTO `oradores` (nombre,apellido,tema) VALUES ('Nicolas','Gomez','JavaScript'),('Martin','Gonzalez','React'),('Micaela','Perez','NodeJS'),('Erica','Garcia','Bases de Datos'),('Rocio','Villalba','Angular'),('Facundo','Rios','.Net'),('Federico','Acosta','Java'),('Camila','Martinez','Java'),('Noelia','Sanchez','React');
 
 
 -- LOGIN
